f6638a94d6defd8bf2fc13477386435c0b66eadf
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-27 19:17:58 +0200

    modification of login form with CSS and StimulusJS style

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import LabelToggleController from "./label_toggle_controller"
+application.register("label-toggle", LabelToggleController)

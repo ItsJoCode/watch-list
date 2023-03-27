a7ae2c956fd3cf50fc2265a0a89382585eed4545
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-27 16:44:15 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # All registration and session for user devise
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Home Page
+  root "lists#index"
+
+  resources :lists, only: %i[index show]
+  namespace :user do
+    resources :lists do
+      resources :bookmarks, only: %i[new create]
+    end
+    resources :bookmarks, only: %i[destroy]
+  end
 end
